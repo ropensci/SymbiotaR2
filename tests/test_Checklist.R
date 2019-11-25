9b320473e5fe3a3fab3e86977045da444f929c61
akoontz11 <austinkoontz11@gmail.com>
2019-11-25 14:57:44 -0700

    Included vcr wrapping for relevant function calls

--- tests/test_Checklist.R
+++ tests/test_Checklist.R
@@ -1,30 +1,38 @@
 # Checklist tests
 
 context("ChecklistProjects")
-test_that("ChecklistProjects",{
-  data <- ChecklistProjects(ID=1)
-  expect_equal(length(data),  19)
-  expect_is(data, "list")
+use_cassette(name="ChecklistProjects_ID", {
+  test_that("ChecklistProjects_ID",{
+    data <- ChecklistProjects(ID=1)
+    expect_equal(length(data),  19)
+    expect_is(data, "list")
+  })
 })
-test_that("ChecklistProjects",{
-  data <- ChecklistProjects(page=1)
-  expect_equal(length(data),  18)
-  expect_is(data, "list")
+use_cassette(name="ChecklistProjects_page", {
+  test_that("ChecklistProjects_page",{
+    data <- ChecklistProjects(page=1)
+    expect_equal(length(data),  18)
+    expect_is(data, "list")
+  })
 })
 
 context("Children")
 #Children
 
 context("Coordinates")
-test_that("Coordinates",{
-  data <- Coordinates(ID=5)
-  expect_equal(length(data),  2)
-  expect_is(data, "double")
+use_cassette(name="Coordinates_ID",{
+  test_that("Coordinates_ID",{
+    data <- Coordinates(ID=5)
+    expect_equal(length(data),  2)
+    expect_is(data, "double")
+  })
 })
-test_that("Coordinates",{
-  data <- Coordinates(page=1)
-  expect_equal(length(data),  25)
-  expect_is(data, "list")
+use_cassette(name="Coordinates_page",{
+  test_that("Coordinates_page",{
+    data <- Coordinates(page=1)
+    expect_equal(length(data),  2)
+    expect_is(data, "list")
+  })
 })
 
 context("Dynamic")
@@ -37,28 +45,36 @@ context("TaxaComments")
 #TaxaComments
 
 context("TaxaLink")
-test_that("TaxaLink",{
-  data <- TaxaLink(ID=5)
-  expect_equal(length(data),  19)
-  expect_is(data, "list")
+use_cassette(name="TaxaLink_ID",{
+  test_that("TaxaLink_ID",{
+    data <- TaxaLink(ID=5)
+    expect_equal(length(data),  19)
+    expect_is(data, "list")
+  })
 })
-test_that("TaxaLink",{
-  data <- TaxaLink(page=1)
-  expect_equal(length(data),  540)
-  expect_is(data, "list")
+use_cassette(name="TaxaLink_page",{
+  test_that("TaxaLink_page",{
+    data <- TaxaLink(page=1)
+    expect_equal(length(data),  540)
+    expect_is(data, "list")
+  })
 })
 
 context("Vouchers")
 #Vouchers
 
 context("Checklists")
-test_that("Checklists",{
-  data <- Checklists(ID=1)
-  expect_equal(length(data),  31)
-  expect_is(data, "list")
+use_cassette(name="Checklists_ID", {
+  test_that("Checklists_ID",{
+    data <- Checklists(ID=1)
+    expect_equal(length(data),  31)
+    expect_is(data, "list")
+  })
 })
-test_that("Checklists",{
-  data <- Checklists(page=1)
-  expect_equal(length(data),  30)
-  expect_is(data, "list")
+use_cassette(name="Checklists_page", {
+  test_that("Checklists_page",{
+    data <- Checklists(page=1)
+    expect_equal(length(data),  30)
+    expect_is(data, "list")
+  })
 })
