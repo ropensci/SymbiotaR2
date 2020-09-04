f5e7e1c51c045df08a837ec35eb78c8dff98a646
akoontz11 <austinkoontz11@gmail.com>
2020-09-04 14:30:58 -0600

    Script to allow for top-level documentation of the package

---
+++ R/SymbiotaR2-package.R
@@ -0,0 +1,22 @@
+#' Downloading data from Symbiota2 portals into R
+#'
+#' SymbiotaR2 allows users to access and download from Symbiota2, 
+#' a content management system for biodiveristy data. It does this 
+#' by pulling resources from an existing SymbiotaR2 portal 
+#' specified by the user. Through specification of those resources, 
+#' users have accessed to a large database of specimen and 
+#' observation data in the R environment. 
+#' 
+#' +Direct users to where they can learn more about setting up a database
+#' +Include reference below
+#' +Improve code example
+#' 
+#' @docType package
+#' @name SymbiotaR2
+#' @aliases SymbiotaR2 package-SymbiotaR2 SymbiotaR2-package
+#' @references 
+#' @examples
+#' url <- "http://a02235015-6.bluezone.usu.edu/api/"
+#' my.Taxa <- Taxa(url, id=12)
+#' str(my.Taxa)
+NULL
