10cccac34111262315a69088bdc0488b06ad524c
akoontz11 <austinkoontz11@gmail.com>
2020-09-25 13:00:02 -0600

    Updated argument order

--- tests/testthat/test_Collection.R
+++ tests/testthat/test_Collection.R
@@ -3,14 +3,14 @@ url <- "http://a02235015-6.bluezone.usu.edu/api/"
 
 context("Categories")
 vcr::use_cassette(name = "Categories_id", {
-  data <- Categories(url = url, id = 1)
+  data <- Categories(id = 1, url = url)
 })
 test_that("Categories_id", {
   expect_equal(length(data), 12)
   expect_type(data, "list")
 })
 vcr::use_cassette(name = "Categories_page", {
-  data <- Categories(url = url, page = 1)
+  data <- Categories(page = 1, url = url)
 })
 test_that("Categories_page", {
   expect_equal(length(data), 110)
@@ -19,14 +19,14 @@ test_that("Categories_page", {
 
 context("Institutions")
 vcr::use_cassette(name = "Institutions_id", {
-  data <- Institutions(url = url, id = 5)
+  data <- Institutions(id = 5, url = url)
 })
 test_that("Institutions_id", {
   expect_equal(length(data), 21)
   expect_type(data, "list")
 })
 vcr::use_cassette(name = "Institutions_page", {
-  data <- Institutions(url = url, page = 1)
+  data <- Institutions(page = 1, url = url)
 })
 test_that("Institutions_page", {
   expect_equal(length(data), 600)
@@ -35,14 +35,14 @@ test_that("Institutions_page", {
 
 context("Stats")
 vcr::use_cassette(name = "Stats_id", {
-  data <- Stats(url = url, id = 1)
+  data <- Stats(id = 1, url = url)
 })
 test_that("Stats_id", {
   expect_equal(length(data), 14)
   expect_type(data, "list")
 })
 vcr::use_cassette(name = "Stats_page", {
-  data <- Stats(url = url, page = 1)
+  data <- Stats(page = 1, url = url)
 })
 test_that("Stats_page", {
   expect_equal(length(data), 390)
@@ -51,14 +51,14 @@ test_that("Stats_page", {
 
 context("Collections")
 vcr::use_cassette(name = "Collections_id", {
-  data <- Collections(url = url, id = 1)
+  data <- Collections(id = 1, url = url)
 })
 test_that("Collections_id", {
   expect_equal(length(data), 37)
   expect_type(data, "list")
 })
 vcr::use_cassette(name = "Collections_page", {
-  data <- Collections(url = url, page = 1)
+  data <- Collections(page = 1, url = url)
 })
 test_that("Collections_page", {
   expect_equal(length(data), 36)
