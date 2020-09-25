15b54efcc3a587d98f56ccd9590dbde285ca267e
akoontz11 <austinkoontz11@gmail.com>
2020-09-25 12:59:50 -0600

    Updated argument order

--- tests/testthat/test_Checklist.R
+++ tests/testthat/test_Checklist.R
@@ -3,14 +3,14 @@ url <- "http://a02235015-6.bluezone.usu.edu/api/"
 
 context("ChecklistProjects")
 vcr::use_cassette(name = "ChecklistProjects_id", {
-  data <- ChecklistProjects(url = url, id = 1)
+  data <- ChecklistProjects(id = 1, url = url)
 })
 test_that("ChecklistProjects_id", {
   expect_equal(length(data), 19)
   expect_type(data, "list")
 })
 vcr::use_cassette(name = "ChecklistProjects_page", {
-  data <- ChecklistProjects(url = url, page = 1)
+  data <- ChecklistProjects(page = 1, url = url)
 })
 test_that("ChecklistProjects_page", {
   expect_equal(length(data), 18)
@@ -19,14 +19,14 @@ test_that("ChecklistProjects_page", {
 
 context("Coordinates")
 vcr::use_cassette(name = "Coordinates_id", {
-  data <- Coordinates(url = url, id = 5)
+  data <- Coordinates(id = 5, url = url)
 })
 test_that("Coordinates_id", {
   expect_equal(length(data), 2)
   expect_type(data, "double")
 })
 vcr::use_cassette(name = "Coordinates_page", {
-  data <- Coordinates(url = url, page = 1)
+  data <- Coordinates(page = 1, url = url)
 })
 test_that("Coordinates_page", {
   expect_equal(length(data), 2)
@@ -35,14 +35,14 @@ test_that("Coordinates_page", {
 
 context("TaxaLink")
 vcr::use_cassette(name = "TaxaLink_id", {
-  data <- TaxaLink(url = url, id = 5)
+  data <- TaxaLink(id = 5, url = url)
 })
 test_that("TaxaLink_id", {
   expect_equal(length(data), 19)
   expect_type(data, "list")
 })
 vcr::use_cassette(name = "TaxaLink_page", {
-  data <- TaxaLink(url = url, page = 1)
+  data <- TaxaLink(page = 1, url = url)
 })
 test_that("TaxaLink_page", {
   expect_equal(length(data), 540)
@@ -51,14 +51,14 @@ test_that("TaxaLink_page", {
 
 context("Checklists")
 vcr::use_cassette(name = "Checklists_id", {
-  data <- Checklists(url = url, id = 1)
+  data <- Checklists(id = 1, url = url)
 })
 test_that("Checklists_id", {
   expect_equal(length(data), 31)
   expect_type(data, "list")
 })
 vcr::use_cassette(name = "Checklists_page", {
-  data <- Checklists(url = url, page = 1)
+  data <- Checklists(page = 1, url =url)
 })
 test_that("Checklists_page", {
   expect_equal(length(data), 30)
